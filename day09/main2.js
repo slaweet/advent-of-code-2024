e98b224b9f061f74ced4a912e520f704008814db
Vít Stanislav <slaweet@gmail.com>
2024-12-09 08:52:42 +0100

    🐎 Speed up day 09 part 2
    The speed-up on my input is 30s -> 3s

--- day09/main2.js
+++ day09/main2.js
@@ -14,13 +14,14 @@ try {
   const disk = diskMap
     .map((length, i) => Array(parseInt(length)).fill(`${i % 2 === 0 ? (i / 2) : '.'}`));
  
-  let rearrangedDisk = JSON.parse(JSON.stringify(disk));
+  const rearrangedDisk = JSON.parse(JSON.stringify(disk));
   for (let i = disk.length - 1; i >= 0; i--) {
     if (disk[i][0] !== '.') {
       for (let j = 0; j < rearrangedDisk.length && disk[i][0] !== rearrangedDisk[j][0]; j++) {
         const lengthDiff = rearrangedDisk[j].length - disk[i].length ;
         if (rearrangedDisk[j][0] === '.' && lengthDiff >= 0) {
-          rearrangedDisk = rearrangedDisk.map((file) => (file.map((block) => block.replace(disk[i][0], '.'))));
+          const indexInRearranged = rearrangedDisk.findIndex((file) => file[0] === disk[i][0]);
+          rearrangedDisk[indexInRearranged] = rearrangedDisk[indexInRearranged].map(() => '.');
           rearrangedDisk.splice(j, 1, disk[i], ...(lengthDiff > 0 ? [rearrangedDisk[j].slice(0, lengthDiff)] : []));
           break;
         }
